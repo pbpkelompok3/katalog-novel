995ab7af7bb99476324bc23586f199fba914e011
Farrel Ibrahim <farrelibrahim04@gmail.com>
2022-11-17 00:00:14 +0700

    Filter added

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -7,88 +7,123 @@
                     <h4 class="text-center">Novels</h4>
                 </v-col>
                 <v-col cols="12" sm="12">
-                    <v-breadcrumbs :items="items" class="justify-center mt-n7 jost" dark>
+                    <v-breadcrumbs :items="items" class="justify-center mt-n7" dark>
                         <template v-slot:divider>
                             <v-icon color="#67557D">mdi-chevron-right</v-icon>
                         </template>
-                        <template v-slot:item="{ item }">
-                            <v-breadcrumbs-item :disabled="item.disabled">
-                              <span :style="`color: ${item.color}`">
-                                {{ item.text.toUpperCase() }}
-                              </span>
-                            </v-breadcrumbs-item>
-                          </template>
                     </v-breadcrumbs>
                 </v-col>
             </v-row>
         </div>
         
-        <v-card tile class="mx-16 card1" color="white">
+        <v-card flat tile class="mx-16 card1" color="white">
             <v-row>
-                <v-col cols="12" sm="8" class="pr-0">
-                    <v-card height="250px" tile flat color="#67557D" class="rounded-lg">
-                        <v-row>
-                            <v-col cols="12" sm="4" class="pl-8 pt-4">
-                                <v-btn color="black" dark class="withoutupercase" small tile>
-                                    new
-                                </v-btn>
-                                <h6 class="white--text mt-8">Raditya Dika</h6>
-                                <h3 class="white--text mt-5">Marmut Merah Jambu</h3>
-                                <v-btn rounded color="#4f3d61" dark class="withoutupercase px-8 mt-10" elevation="5">
-                                    More info
-                                </v-btn>
-                            </v-col>
-                            <v-col cols="12" sm="8">
-                                <v-img src="../assets/logo.png" max-height="200" max-width="400" contain class="mt-1"></v-img>
-                            </v-col>
-                        </v-row>
-                    </v-card>
-                </v-col>
-                <v-col cols="12" sm="4" class="pl-0">
-                    <v-card height="250px" tile flat color="#6857E1" class="rounded-lg">
+                <v-col cols="12" sm="6" class="pr-0">
+                    <v-card height="250px" tile elevation="3" color="rgb(103, 85, 125, 0.7)" class="rounded-lg">
                         <v-row>
                             <v-col cols="12" sm="6" class="pl-8 pt-4">
-                                <v-btn color="black" dark class="withoutupercase" small tile>
-                                    new
-                                </v-btn>
-                                <h6 class="white--text mt-8">Tere Liye</h6>
-                                <h3 class="white--text mt-8">Bumi</h3>
-                                <v-btn rounded color="#8375E7" dark class="withoutupercase px-8 mt-3">
-                                    More Info
-                                </v-btn>
+                                <v-btn color="black" dark class="withoutupercase" small tile>new addition !</v-btn>
+                                <h6 class="white--text mt-8 text-left">Andrea Hirata</h6>
+                                <h3 class="white--text mt-5 text-left">Laskar Pelangi</h3>
+                                <!-- <v-btn rounded color="#826c9e" dark class="withoutupercase px-8 mt-10 ml-0" elevation="5">More info</v-btn> -->
                             </v-col>
                             <v-col cols="12" sm="6">
-                                <v-img src="../assets/logo.png" max-height="400" max-width="420" contain class="mt-1"></v-img>
+                                <v-img src="../assets/Laskar Pelangi.jpg" max-height="220" contain class="mt-1 new"></v-img>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-col>
+                <v-col cols="12" sm="6" class="pl-0">
+                    <v-card height="250px" tile elevation="3" color="rgb(199, 174, 149, 0.7)" class="rounded-lg">
+                        <v-row>
+                            <v-col cols="12" sm="6" class="pl-8 pt-4">
+                                <v-btn color="black" dark class="withoutupercase" small tile>new addition !</v-btn>
+                                <h6 class="white--text mt-8 text-left">Tere Liye</h6>
+                                <h3 class="white--text mt-8 text-left">Bumi</h3>
+                                <!-- <v-btn rounded color="#8375E7" dark class="withoutupercase px-8 mt-10 ml-0" elevation="5">More Info</v-btn> -->
+                            </v-col>
+                            <v-col cols="12" sm="6">
+                                <v-img src="../assets/Bumi.jpg" max-height="220" contain class="mt-1"></v-img>
                             </v-col>
                         </v-row>
                     </v-card>
                 </v-col>
             </v-row>
-            <v-row>
-                <v-col cols="12" sm="3" class="mt-n6 pr-0">
-                    <v-toolbar-title><strong>Filter</strong></v-toolbar-title>
-                    <v-spacer></v-spacer>
-                    <v-divider vertical></v-divider>
-                    <v-btn icon class="ml-1">
-                        <v-icon>
-                            mdi-sync
-                        </v-icon>
-                    </v-btn>
-                </v-col>
-                <v-col cols="12" sm="3" class="mt-n6 px-0">
-                    <v-toolbar flat outlined>
-                        <v-toolbar-title>
-
-                        </v-toolbar-title>
-                    </v-toolbar>
-                </v-col>
+            <v-row flat class="transparent">
+                <v-col></v-col>
             </v-row>
+            <v-row flat class="transparent">
+                <v-col></v-col>
+            </v-row>
+            <v-row>
+                <v-col cols="12" sm="12" class="mt-n6 pr-0">
+                    <h2 class="filter jost">Filter</h2>
+                    <!-- <v-toolbar>
+                        <v-toolbar-title><strong>Filter</strong></v-toolbar-title>
+                        <v-divider vertical></v-divider>
+                        <v-spacer></v-spacer>
+                        <v-divider vertical></v-divider>
+                        <v-btn icon class="ml-1">
+                            <v-icon color="#67557D">mdi-sync</v-icon>
+                        </v-btn>
+                </v-toolbar> -->
+            </v-col>
+            <!-- <v-col cols="12" sm="3" class="mt-n6 px-0">
+                <v-toolbar flat outlined>
+                    <v-toolbar-title>Short by: <span>Judul</span></v-toolbar-title>
+                </v-toolbar>
+            </v-col> -->
+            <!-- <v-col cols="12" sm="6" class="mt-n6 px-0">
+                <v-toolbar flat outlined class="">
+                    <v-toolbar-title>SHOW: <span>12</span></v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-btn icon class="mr-4">
+                        <v-icon color="black">mdi-menu-down</v-icon>
+                    </v-btn>
+                </v-toolbar>
+            </v-col> -->
+            <!-- <v-col cols="12" sm="3" class="mt-n6 pl-0">
+                <v-toolbar flat outlined>
+                    <v-btn icon class="mr-1">
+                        <v-icon color="#3853D8">mdi-apps</v-icon>
+                    </v-btn>
+                    <v-divider vertical></v-divider>
+                    <v-btn icon class="mx-1">
+                        <v-icon color="grey">mdi-format-list-bulleted</v-icon>
+                    </v-btn>
+                    <v-divider vertical></v-divider>
+                    <v-toolbar-title class="ml-2"><strong>COMPARE :</strong></v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-badge color="#3853D8" content="3" class="mr-2"></v-badge>
+                </v-toolbar>
+            </v-col> -->
+            <v-col cols="3" class="py-0 pr-0 mt-n3">
+                <v-card flat outlined tile>
+                    <v-toolbar flat>
+                        <strong>Genre</strong>
+                        <v-spacer></v-spacer>
+                        <v-icon color="black" class="mr-2">mdi-chevron-down</v-icon>
+                    </v-toolbar>
+                    <v-list dense class="mt-n5">
+                        <v-list-item v-for="book in books" :key="book.title">
+                            <v-list-item-content>
+                                <v-list-item-title v-text="book.title" class="text-left ml-3"></v-list-item-title>
+                            </v-list-item-content>
+                            <v-list-item-action>
+                                <v-list-item-subtitle v-text="book.count" class="text-right mr-3"></v-list-item-subtitle>
+                            </v-list-item-action>
+                        </v-list-item>
+                    </v-list>
+                </v-card>
+            </v-col>
+        </v-row>
         </v-card>
     </v-container>
 </template>
 
 
 <script>
+
 export default {
     data() {
         return {
@@ -112,6 +147,21 @@ export default {
                     color: "#67557D",
                 },
             ],
+
+            books: [
+                {
+                    title: 'Sci-fi',
+                    count: '20',
+                },
+                {
+                    title: 'Fantasy',
+                    count: '10',
+                },
+                {
+                    title: 'Romance',
+                    count: '33', 
+                }
+            ]
         }
     },
 }
@@ -125,9 +175,15 @@ export default {
 .v-breadcrumbs {
     color: #67557D;
 }
+.v-breadcrumbs__item:link, .v-breadcrumbs__item:visited,.v-breadcrumbs__item:hover, .v-breadcrumbs__item:active {
+    font-family: 'Jost', cursive;
+    color: #67557d;
+}
 
 .v-btn.withoutupercase {
-    text-decoration: none;
+    display: flex;
+    font-family: 'Jost', cursive;
+    cursor: default;
 }
 .v-chip.v-size--default {
     border-radius: 50px !important;
@@ -153,6 +209,18 @@ export default {
 .jost {
     font-family: 'Jost', cursive;
 }
-
+.transparent {
+    background-color: transparent !important;
+    border-color: transparent !important;
+}
+.filter {
+    display: flex;
+    margin: -10px 0 0 10px;
+    margin-bottom: 5px;
+}
+.new {
+    display: flex;
+    right: 0;
+}
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,23 @@
 <template>
   <v-app>
     <NavBar class="index" />
-    <FilTer />
     <DashBoard />
-    <FooTer />
+    <!-- <FooTer /> -->
   </v-app>
 </template>
 
 <script>
 // @ is an alias to /src
 import NavBar from '@/components/NavBar.vue'
-import FooTer from '@/components/FootEr.vue'
+// import FooTer from '@/components/FootEr.vue'
 import DashBoard from '@/components/DashBoard.vue'
-import FilTer from '@/components/FilTer.vue'
 
 export default {
   name: 'HomeView',
   components: {
     NavBar,
-    FooTer,
+    // FooTer,
     DashBoard,
-    FilTer,
   },
 }
 </script>
