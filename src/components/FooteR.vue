e5e966de8e6f0c401441b96fdfc116305007361c
Farrel Ibrahim <farrelibrahim04@gmail.com>
2022-11-21 12:09:49 +0700

    komponen login dipisah

--- src/components/FooteR.vue
+++ src/components/FooteR.vue
@@ -81,24 +81,7 @@
 
                     <v-overlay :z-index="100" :value="overlay">
                         <div class="logInOverlay" v-click-outside="onClickOutside">
-                            <v-form ref="form" v-model="valid" lazy-validation @submit.prevent="submitForm">
-                                <v-container class="formContainer">
-                                    <h2 class="overlayHeader pt-8 pb-5">Login Admin</h2>
-                                    <v-row>
-                                        <v-icon large class="pt-13 ml-5">fa fa-circle-user</v-icon>
-                                        <v-text-field v-model="loginValues.username" :rules="usernameRules" label="Username" required class="pt-16 mx-5"></v-text-field>
-                                    </v-row>
-                                    <v-row>
-                                        <v-icon large class="pt-5 ml-5">fa fa-lock</v-icon>
-                                        <v-text-field v-model="loginValues.password" type="password" label="Password" required class="pt-8 mx-5"></v-text-field>
-                                    </v-row>
-                                    <v-row>
-                                        <v-btn type="submit" class="loginButon" color="white">
-                                            <div class="loginText">Masuk</div>
-                                        </v-btn>
-                                    </v-row>
-                                </v-container>
-                            </v-form>
+                            <LogIn />
                         </div>
                     </v-overlay>
         
@@ -115,6 +98,8 @@
 
 
 <script>
+import LogIn from '@/components/LogIn.vue'
+
 export default {
     data() {
         return {
@@ -127,25 +112,16 @@ export default {
                 'mdi-pinterest',
             ],
             overlay: false,
-            valid: true,
-            loginValues: {
-                username: '',
-                password: '',
-            },
-            usernameRules: [
-                v => !!v || 'Userame is required',
-                v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-            ],
         }
     },
     methods: {
         onClickOutside() {
             this.overlay = false
         },
-        submitForm() {
-            console.log('Login Values', this.loginValues)
-        }
-    }
+    },
+    components: {
+        LogIn,
+    },
 }
 </script>
 
---
+++ src/components/LogIn.vue
@@ -0,0 +1,51 @@
+<template lang="">
+    <div>
+        <v-form ref="form" v-model="valid" lazy-validation @submit.prevent="submitForm">
+            <v-container class="formContainer">
+                <h2 class="overlayHeader pt-8 pb-5">Login Admin</h2>
+                <v-row>
+                    <v-icon large class="pt-13 ml-5">fa fa-circle-user</v-icon>
+                    <v-text-field v-model="loginValues.username" :rules="usernameRules" label="Username" required class="pt-16 mx-5"></v-text-field>
+                </v-row>
+                <v-row>
+                    <v-icon large class="pt-5 ml-5">fa fa-lock</v-icon>
+                    <v-text-field v-model="loginValues.password" type="password" label="Password" required class="pt-8 mx-5"></v-text-field>
+                </v-row>
+                <v-row>
+                    <v-btn type="submit" class="loginButon" color="white">
+                        <div class="loginText">Masuk</div>
+                    </v-btn>
+                </v-row>
+            </v-container>
+        </v-form>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            valid: true,
+            loginValues: {
+                username: '',
+                password: '',
+            },
+            usernameRules: [
+                v => !!v || 'Userame is required',
+                v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+            ],
+        }
+    },
+    methods: {
+        submitForm() {
+            console.log('Login Values', this.loginValues)
+        }
+    }
+}
+</script>
+
+
+<style lang="scss">
+    
+</style>
